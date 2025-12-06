11cb28471243cc56ad0d506a146807fb6c7d81b6
Gabriel Quispe Barra <120265890+eltatia@users.noreply.github.com>
2025-12-06 13:02:47 -0500

    Add missing layout imports to calendar screen

--- app/src/main/java/com/gabriel/controlfinanciero/ui/screens/CalendarioScreen.kt
+++ app/src/main/java/com/gabriel/controlfinanciero/ui/screens/CalendarioScreen.kt
@@ -1,20 +1,22 @@
 package com.gabriel.controlfinanciero.ui.screens
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.align
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
